f3e8661cc19edda15be87892c9c5a4c62f805b06
bauren_a <bauren_a@epitech.eu>
2016-02-15 14:29:31 +0100

    Modifié

--- get_next_line.c
+++ get_next_line.c
@@ -5,7 +5,7 @@
 ** Login   <bauren_a@epitech.net>
 **
 ** Started on  Tue Jan 12 15:12:31 2016 Arthur
-** Last update Fri Jan 15 15:58:57 2016 Arthur
+** Last update Mon Feb 15 14:27:21 2016 Arthur
 */
 
 #include <stdlib.h>
@@ -13,92 +13,85 @@
 #include <unistd.h>
 #include "get_next_line.h"
 
-int     str_len(char *str)
+int     sl(char *str, char c)
 {
   int   i;
 
   i = -1;
   if (str == NULL)
     return (0);
-  while (str[++i] != '\0');
-  return (i);
+  while (str[++i])
+    if (str[i] == c)
+      return (i);
+  return (c == '\0' ? i : -1);
 }
 
-char    *my_strcat(char *str1, char *str2)
+char    *mstc(char *str1, char *str2, int n)
 {
   int	i;
   int	j;
+  int	s;
   int	len;
   int	len2;
   char	*ret;
 
-  len = str_len(str1);
-  len2 = str_len(str2);
-  if (str2 == NULL || (ret = malloc(sizeof(char) * (len + len2 + 1))) == NULL)
+  len = sl(str1, '\0');
+  len2 = sl(str2, '\0');
+  s = len + len2;
+  if (n < size && n != -1)
+    size = n;
+  if (str2 == NULL || (ret = malloc(sizeof(char) * (size + 1))) == NULL)
     return (NULL);
   i = 0;
-  while (str1 != NULL && str1[i] != '\0')
+  while (str1 != NULL && str1[i] && (n == -1 ? (1 == 1) : (i < n)))
     ret[i] = str1[i++];
   j = 0;
-  while (str2[j] != '\0')
+  while (str2[j] && (n == -1 ? (1 == 1) : (i < n)))
     ret[i++] = str2[j++];
   ret[i] = 0;
   return (ret);
 }
 
-void	*free_and_null(void *ptr)
+void	*fan(void *ptr)
 {
   free(ptr);
   return (NULL);
 }
 
-char	*load_file(const int fd)
+char	*rl(char *sv, char *buff, int s)
 {
-  int	size[2];
-  char	*result;
   char	*tmp;
-  char	buffer[READ_SIZE + 1];
 
-  result = NULL;
-  size[1] = 0;
-  while ((size[0] = read(fd, buffer, READ_SIZE)) > 0)
-    {
-      buffer[size[0]] = 0;
-      tmp = result;
-      if ((result = my_strcat(result, buffer)) == NULL)
-	return (free_and_null(tmp));
-      free(tmp);
-      size[1] += size[0];
-    }
-  if (result != NULL)
-    result[size[1]] = '\0';
-  return (result);
+  buffer[s] = 0;
+  tmp = sv;
+  sv = mstc(sv, buffer, -1);
+  free(tmp);
+  return (sv);
 }
 
 char		*get_next_line(const int fd)
 {
-  int		i;
+  static char	*sv = NULL;
   char		*tmp;
   char		*line;
-  static int	old_fd = -1;
-  static char	*file = NULL;
+  int		l;
+  int		s;
+  char		buffer[READ_SIZE + 1];
 
-  i = -1;
-  if (file == NULL)
-    file = load_file(fd);
-  if (file == NULL)
+  s = 0;
+  l = sl(sv, '\n');
+  while (l <= 0 && (s = read(fd, buffer, READ_SIZE)) > 0)
+    sv = rl(sv, buffer, s);
+  if (s < 0)
     return (NULL);
-  while (file[++i] != '\n' && file[i] != '\0');
-  if ((line = malloc(sizeof(char) * i + 1)) == NULL)
-    return (NULL);
-  i = 0;
-  while (file[i] != '\n' && file[i] != '\0')
-    line[i] = file[i++];
-  line[file[i] == '\0' ? i : i++] = 0;
-  tmp = file;
-  file = my_strcat(NULL, &file[i]);
-  free(tmp);
-  if (str_len(file) == 0)
-    file = free_and_null(file);
+  if (sl(sv, '\0') == 0 && s == 0)
+    return (line = fan(sv));
+  else if (sv != NULL && sl(sv, '\0') != 0)
+    {
+      line = mstc(NULL, save, ((l == -1) ? l : l++));
+      tmp = save;
+      save = mstc(NULL, ((l == -1) ? NULL : &save[l]), -1);
+      free(tmp);
+    }
   return (line);
 }
